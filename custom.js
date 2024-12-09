8ab0ef453e0758e1162c29fb2164cc996b28c1cc
Md.Naimur Rahman <74141098+Akashbk201@users.noreply.github.com>
2024-12-10 02:12:40 +0600

    Update custom.js

---
+++ custom.js
@@ -0,0 +1,53 @@
+let name = "John";
+
+console.log("Hello", name, "!");
+
+let a = 12;
+let b = 15;
+
+console.log("Answer is :", a + b);
+
+var age = 27;
+console.log("I am", age, "years old");
+
+let math = 78;
+let english = 80;
+let bangla = 85;
+
+console.log("Avarage of 3 Subject :", (math + english + bangla) / 3);
+
+const newVariable = "camelCase";
+const new_variable = "snake_case";
+
+console.log(newVariable);
+console.log(new_variable);
+
+let word = "Hello";
+
+if (word == "Hello") {
+  console.log("Word Founded");
+} else {
+  console.log("Could not found the Word!");
+}
+
+let input_name = "Sara";
+let input_lang = "Python";
+let input_year = "2018";
+
+console.log(
+  `${input_name}, you have been coding in ${input_lang} since ${input_year}. `
+);
+
+let text = "Text";
+let num = 40;
+let darkmood = true;
+let whitemood = false;
+let new_text;
+let new_var = null;
+
+console.log("Type of this variable :", typeof text);
+console.log("Type of this variable :", typeof num);
+console.log("Type of this variable :", typeof darkmood);
+console.log("Type of this variable :", typeof whitemood);
+console.log("Type of this variable :", typeof new_text);
+console.log("Type of this variable :", typeof new_var);
